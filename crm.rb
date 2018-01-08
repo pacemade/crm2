bf809ea5dbe7d66054eaa4f7230b0e861ecba649
Danny Chang <dannychachang@gmail.com>
2018-01-08 13:52:17 -0500

    Add DELETE page, edit redirects to View Contact

--- crm.rb
+++ crm.rb
@@ -14,16 +14,6 @@ get '/contacts/new' do
   erb :new
 end
 
-get '/contacts/:id' do
-  id = params[:id].to_i
-  @contact = Contact.find_by(id: id)
-  if @contact != nil
-    erb :show_contact
-  else
-    erb :four_o_four
-  end
-end
-
 post '/contacts' do
   Contact.create(
     first_name: params[:first_name],
@@ -32,6 +22,50 @@ post '/contacts' do
     note:       params[:note]
   )
   redirect to('/contacts')
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact != nil
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 after do
