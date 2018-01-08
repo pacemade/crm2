a32735e1dfa9bb6928d765bcd01a56245ee5e9a1
Danny Chang <dannychachang@gmail.com>
2018-01-08 13:16:51 -0500

    Move get 'contact/new' up, cannot be seen as /:id taking over

--- crm.rb
+++ crm.rb
@@ -10,6 +10,10 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/new' do
+  erb :new
+end
+
 get '/contacts/:id' do
   id = params[:id].to_i
   @contact = Contact.find_by(id: id)
@@ -20,8 +24,14 @@ get '/contacts/:id' do
   end
 end
 
-get '/new' do
-  erb :new
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
 end
 
 after do
