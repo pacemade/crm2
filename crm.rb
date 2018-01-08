165fc76a8661568509d26e0d3a1b3ed116edd3da
Danny Chang <dannychachang@gmail.com>
2018-01-08 13:05:49 -0500

    Add new.erb and create form

--- crm.rb
+++ crm.rb
@@ -2,5 +2,28 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-  erb:index
+  erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  id = params[:id].to_i
+  @contact = Contact.find_by(id: id)
+  if @contact != nil
+    erb :show_contact
+  else
+    erb :four_o_four
+  end
+end
+
+get '/new' do
+  erb :new
+end
+
+after do
+  ActiveRecord::Base.connection.close
 end
